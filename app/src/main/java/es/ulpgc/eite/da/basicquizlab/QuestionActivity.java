d8ba3841020f613d3aab8bfaa1b8a251d5525612
Luis Hernandez <lhdez@iuma.ulpgc.es>
2024-02-25 19:19:19 +0000

    refactorizando codigo

--- app/src/main/java/es/ulpgc/eite/da/basicquizlab/QuestionActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/QuestionActivity.java
@@ -35,6 +35,42 @@ public class QuestionActivity extends AppCompatActivity {
     enableLayoutButtons();
   }
 
+  /*
+  private void enableLayoutButtons() {
+
+    trueButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        onTrueButtonClicked(v);
+      }
+    });
+
+    falseButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        onFalseButtonClicked(v);
+      }
+    });
+
+    nextButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        onNextButtonClicked(v);
+      }
+    });
+
+    cheatButton.setOnClickListener(new View.OnClickListener() {
+
+      @Override
+      public void onClick(View v) {
+        onCheatButtonClicked(v);
+      }
+    });
+  }
+  */
 
   private void enableLayoutButtons() {
 
